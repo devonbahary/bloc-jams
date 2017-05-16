5bae071a23bda6075fe2377a6ce409821d5ad6e9
devonbahary <devonbahary@gmail.com>
2017-05-15 18:45:41 -0700

    Checkpoint 9 DOM Scripting: Events | Completed

--- scripts/landing.js
+++ scripts/landing.js
@@ -4,9 +4,9 @@
       landing.js
 */
 
-var animatePoints = function() {
+var points = document.getElementsByClassName('point');
 
-  var points = document.getElementsByClassName('point');
+var animatePoints = function(points) {
 
   var revealPoint = function(i) {
     points[i].style.opacity = 1;
@@ -19,5 +19,21 @@ var animatePoints = function() {
     revealPoint(i);
   }
 };
+
+window.onload = function() {
+  // automatically animate the points on a tall screen where scrolling can't trigger the animation
+  if (window.innerHeight > 950) {
+    animatePoints(points);
+  }
+  
+  var sellingPoints = document.getElementsByClassName('selling-points')[0];
+  var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+
+  window.addEventListener('scroll', function(event) {
+    if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+      animatePoints(points);
+    }
+  });
+}
 
 animatePoints();
