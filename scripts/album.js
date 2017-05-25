e55b5f2aa45671cbe00d3f8c7d649a012de9e638
devonbahary <devonbahary@gmail.com>
2017-05-24 19:44:33 -0700

    Checkpoint 20 jQuery: Buzz Library | Completed

--- scripts/album.js
+++ scripts/album.js
@@ -30,13 +30,18 @@ var createSongRow = function(songNumber, songName, songLength) {
   		// Switch from Play -> Pause button to indicate new song is playing.
   		$(this).html(pauseButtonTemplate);
       setSong(songNumber);
+      currentSoundFile.play();
       updatePlayerBarSong();
   	} else if (currentlyPlayingSongNumber === songNumber) {
-  		// Switch from Pause -> Play button to pause currently playing song.
-  		$(this).html(playButtonTemplate);
-      $('main-controls .play-pause').html(playerBarPlayButton);
-  		currentlyPlayingSongNumber = null;
-      currentSongFromAlbum = null;
+      if (currentSoundFile.isPaused()) {
+        $(this).html(pauseButtonTemplate);
+        $('main-controls .play-pause').html(playerBarPauseButton);
+        currentSoundFile.play();
+      } else {
+        $(this).html(playButtonTemplate);
+        $('main-controls .play-pause').html(playerBarPlayButton);
+        currentSoundFile.pause();
+      }
   	}
   };
 
@@ -97,9 +102,27 @@ var getSongNumberCell = function(number) {
 // setSong(songNumber)
 //  => sets global vars 'currentlyPlayingSongNumber' and 'currentSongFromAlbum'
 // according to 'songNumber'
+//  => creates a Buzz 'sound' object
 var setSong = function(songNumber) {
+  if (currentSoundFile) {
+    currentSoundFile.stop();
+  }
+
   currentlyPlayingSongNumber = parseInt(songNumber);
   currentSongFromAlbum = currentAlbum.songs[songNumber - 1];
+
+  currentSoundFile = new buzz.sound(currentSongFromAlbum.audioUrl, {
+    formats: [ 'mp3' ],
+    preload: true
+  });
+
+  setVolume(currentVolume);
+};
+
+var setVolume = function(volume) {
+  if (currentSoundFile) {
+    currentSoundFile.setVolume(volume);
+  }
 };
 
 // trackIndex(album, song)
@@ -124,6 +147,7 @@ var nextSong = function() {
 
   // Set a new current song
   setSong(currentSongIndex + 1);
+  currentSoundFile.play();
 
   // Update the Player Bar information
   updatePlayerBarSong();
@@ -151,6 +175,7 @@ var previousSong = function() {
 
   // Set a new current song
   setSong(currentSongIndex + 1);
+  currentSoundFile.play();
 
   // Update the Player Bar information
   updatePlayerBarSong();
@@ -184,6 +209,8 @@ var playerBarPauseButton = '<span class="ion-pause"></span>';
 var currentAlbum = null;
 var currentlyPlayingSongNumber = null;
 var currentSongFromAlbum = null;
+var currentSoundFile = null;
+var currentVolume = 80;
 
 var $previousButton = $('.main-controls .previous');
 var $nextButton = $('.main-controls .next');
